892577d6347f75b99a60cf33ba38a605d23090e4
laytonglee <laytongly@gmail.com>
2026-01-25 22:24:37 +0700

    Add User Repository

---
+++ src/main/java/com/example/springsecurity/enumeration/Role.java
@@ -0,0 +1,7 @@
+package enumeration;
+
+public enum Role {
+    USER,
+    ADMIN,
+    MANAGER
+}
---
+++ src/main/java/com/example/springsecurity/model/User.java
@@ -0,0 +1,54 @@
+package com.example.springsecurity.model;
+
+import enumeration.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="users")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true,nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role = Role.USER;
+
+    private boolean enabled = true;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+}
